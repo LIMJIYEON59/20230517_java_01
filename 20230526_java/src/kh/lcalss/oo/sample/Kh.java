bd04e1cad7c26361bb8917d5b08d5f5821bbfad1
LIMJIYEON59 <chayi2110@gmail.com>
2023-06-02 17:55:32 +0900

    Exception IO(File writer)

--- 20230526_java/src/kh/lcalss/oo/sample/Kh.java
+++ 20230526_java/src/kh/lcalss/oo/sample/Kh.java
@@ -10,8 +10,36 @@ public class Kh implements TestInterface, Serializable {
 // static final long serialVersionUID = 1L; 
 
 	public void KhSpecial() {
-		// TODO Auto-generated method stub
+		String str = "11111222222222";
+//		int c = Integer.parseInt(str);
+		Car[] carArr = new Car[3];
+		try {
+			System.out.println("1");
+			// carArr[0] =new Car();
+			carArr[4].setPrice(5000); // ArrayIndexOutOfBoundsException
+			System.out.println("2");
+			carArr[0].setPrice(5000); // NullpointerException
+			int a = Integer.parseInt(str); // NumberFormaException
+			// 10 / 0; 에러남. 뒤에 0의 값이 올 수 없음
+			int c = 10 / 0; // ArithmeticException
+			c = 0 / 10;
+		} catch (NullPointerException e) {
+			System.out.println("예기치 못한 오류 발생하였습니다. 잠시 후 다시 시도해주세요.");
+			e.printStackTrace();
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("데이터 범위를 벗어났습니다.");
+			e.printStackTrace();
+		} catch (NumberFormatException e) { // (Exception e)
+			System.out.println(Integer.MAX_VALUE + "까지의 정수만 입력해주세요");
+			e.printStackTrace();
+		} catch (ArithmeticException e) {
+			System.out.println("0으로 나눈 수는 나타낼 수 없습니다.");
+			e.printStackTrace();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 
+		System.out.println("=====여기가 마지막=========");
 	}
 
 	@Override
